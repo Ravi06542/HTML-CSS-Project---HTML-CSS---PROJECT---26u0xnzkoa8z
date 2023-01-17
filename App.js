6b2baf3eb66c4e46682cca30206bce8db9a4f6b4
Ravi06542 <ravishekher06542@gmail.com>
2023-01-17 22:23:09 +0530

    mobile navigation

--- App.js
+++ App.js
@@ -1,1 +1,15 @@
+const images=document.querySelectorAll(".image")
+const list = document.querySelectorAll("ul li")
 
+list.forEach((el,i)=>{
+    el.addEventListener('click',()=>{
+        images.forEach((el)=>{
+            el.classList.remove("show")
+        })
+        list.forEach((el)=>{
+            el.classList.remove("active")
+        })
+        el.classList.add("active")
+        images[i].classList.add("show")
+    })
+})
